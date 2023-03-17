e83ab8b4dc38752a76527b6d063ee900017749af
malikahhhh <aminamumin606@gmail.com>
2023-03-16 22:42:47 -0700

    pole

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
-#include<stdio.h>
-/**
- * main -Entry point
- * return: always 0 (success)
- */
-int main(void)
-{
-	/*Code to be executed*/
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;/*Successful execution of the program*\
+#include <stdio.h>
+
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
