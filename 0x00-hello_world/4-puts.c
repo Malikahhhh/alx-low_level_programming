055b4a1dbac67bc84c7bfbd438f73281c493ba2b
malikahhhh <aminamumin606@gmail.com>
2023-03-16 22:39:55 -0700

    odd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * Main -Entry point
+ * main -Entry point
  * return: always 0 (success)
  */
 int main(void)
 {
 	/*Code to be executed*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);/*Successful execution of the program*\
+	return 0;/*Successful execution of the program*\
 }
