3cb0f2a04ab8db7fc6e3a52f1ab82ad05c68a02a
malikahhhh <aminamumin606@gmail.com>
2023-03-16 22:15:33 -0700

    dried up

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
+
 /**
- * main -A c program that prints a line to the standard error
+ * main - A C program that prints a line to the standard error
  *
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
-	/*code goes here*/
-	fputs("and that piece of art is useful\"
-		- Dora Korpar, 2015-10-19\n", stdout);
-	return (1);/*Successfulexecution of the program*/
+{
+    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+    return (1);
 }
