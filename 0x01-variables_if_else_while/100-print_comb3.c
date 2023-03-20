9a3c5290baa88e090ddeacaf2ba47e60cfa4b616
malikahhhh <aminamumin606@gmail.com>
2023-03-19 21:20:44 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 /**
- * main - function to print combination of two numbers
- *
- * Return : 0 on success
+ * main - prints combination of two digits.
+ * Return : 0 on success.
  */
 int main(void)
 {
 	int i = '0';
 	int j = '0';
 
-	while(i <= '9')
+	while (i <= '9')
 	{
 		while (j <= '9')
 		{
-			if(!(i>j) || i==j)
+			if (!(i > j) || i == j)
 			{
 				putchar(i);
 				putchar(j);
-				if(i == '8' && j == '9')
+				if (i == '8' && j == '9')
 				{
 					putchar('\n');
 				}
