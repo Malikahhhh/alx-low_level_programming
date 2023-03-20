ec67745e2f61967346e91625b8797a7c2e0bb017
malikahhhh <aminamumin606@gmail.com>
2023-03-20 14:53:43 -0700

    success is hard to get at

--- 0x01-variables_if_else_while/101-quote.c
+++ 0x01-variables_if_else_while/101-quote.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main - entry
+ * main - Entry point
  *
  * Description: print a quote using write function
- * write(int fd,const void *buf,size_t count)
+ *  ssize_t write(int fd,const void *buf,size_t count);
  *
- * Return: 1(unsuccessful)
+ * Return: 1(error).
  */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quo.59);
+	write(1, quo. 59);
 	return (1);
 }
